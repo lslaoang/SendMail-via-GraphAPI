4fbef8cc1af0536730024177bb0e8249a1a36ac9
Laurel Lao Ang <laurellaoang@gmail.com>
2023-01-10 15:39:32 +0800

    Initial commit

---
+++ src/main/java/com/lao/mailgraph/SendMailViaGraphApiApplication.java
@@ -0,0 +1,13 @@
+package com.lao.mailgraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendMailViaGraphApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SendMailViaGraphApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lao/mailgraph/SendMailViaGraphApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lao.mailgraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendMailViaGraphApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
